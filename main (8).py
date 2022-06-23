a311132466cddb332ee2760f3827a3b3b39b8fb6
Vidhiya S B <98318069+VidhiyaSB@users.noreply.github.com>
2022-06-23 17:47:15 +0530

    Add files via upload

---
+++ main (8).py
@@ -0,0 +1,15 @@
+#Code by Vidhiya S B
+n=int(input())
+sum=0
+for i in range(1,n):
+    if(n%i==0):
+        sum=sum+i
+if(sum==n):
+    print("Perfect Number") 
+else:
+    print("Not a perfect Number")
+    
+ 
+ 
+ 
+       
